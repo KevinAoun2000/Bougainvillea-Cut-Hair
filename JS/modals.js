de5f83b79a2e48dd817409b6a8d45afabd5976a3
KevinAoun2000 <kevin.aoun2000@gmail.com>
2026-07-04 23:35:15 -0500

    Archivo JS para que los modales aparezcan  automáticamente

---
+++ JS/modals.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("FirstModal");
+    modal.showModal();
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById("SecondModal");
+    modal.showModal();
+});
